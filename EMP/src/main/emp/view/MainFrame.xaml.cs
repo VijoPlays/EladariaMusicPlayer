2a41a781ca380dcacb46ad1f7b6a61fb3c91bc5a
vijo <blfasan@yahoo.de>
2020-06-23 16:55:12 +0200

    Fixed slider, visually it will still move regardless of mouse over or not

--- EMP/src/main/emp/view/MainFrame.xaml.cs
+++ EMP/src/main/emp/view/MainFrame.xaml.cs
@@ -9,35 +9,60 @@ namespace EMP.main.emp.view
     public partial class MainFrame
     {
         private static readonly MediaPlayer mediaPlayer = new MediaPlayer();
-        private static bool playing, dragStarted;
+        private static bool playing, dragStarted, mouseOverVol;
+        private double sliderVal = 0;
 
         public MainFrame()
         {
             InitializeComponent();
             WindowStartupLocation = WindowStartupLocation.CenterScreen;
             Title = "Eladaria Music Player";
-            
-            
+
+            SliderVolume.MouseEnter += mouseOverEnterVol;
+            SliderVolume.MouseLeave += mouseOverLeaveVol;
+
             menuBar.setMediaPlayer(mediaPlayer); //REMOVE
         }
+
         //TODO: Only slide when mouse over slider
         private void ActionListenerSliderValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            if (!dragStarted) 
-                mediaPlayer.Volume = SliderVolume.Value / 100;
+            if (mouseOverVol)
+                if (!dragStarted)
+                    mediaPlayer.Volume = SliderVolume.Value / 100;
         }
 
         private void ActionListenerSliderDragStarted(object sender, DragStartedEventArgs e)
         {
-            dragStarted = true;
+            if (mouseOverVol)
+                dragStarted = true;
+            else
+                sliderVal = SliderVolume.Value;
         }
 
         private void ActionListenerSliderDragCompleted(object sender, DragCompletedEventArgs e)
         {
-            mediaPlayer.Volume = SliderVolume.Value / 100;
-            dragStarted = false;
+            if (mouseOverVol)
+            {
+                mediaPlayer.Volume = SliderVolume.Value / 100;
+                dragStarted = false;
+            }
+            else
+            {
+                SliderVolume.Value = sliderVal; //TODO: Slider Value does not reset if !MouseOver
+            }
         }
         
+        private void mouseOverEnterVol(object sender, EventArgs e)
+        {
+            mouseOverVol = true;
+        }
+
+        private void mouseOverLeaveVol(object sender, EventArgs e)
+        {
+            mouseOverVol = false;
+        }
+
         public MediaPlayer getMediaPlayer() //REMOVE
         {
             return mediaPlayer;
