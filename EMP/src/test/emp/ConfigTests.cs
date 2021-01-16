04c810cc853b60a4213a844722e7b72f314bc157
vijo <blfasan@yahoo.de>
2021-01-16 14:48:00 +0100

    Added first test

---
+++ EMP/src/test/emp/ConfigTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace EMP.test.emp
+{
+    [TestFixture]
+    public class ConfigTests
+    {
+        
+        [TestCase]
+        public void testThis()
+        {
+            string a = "";
+            string b = a;
+            Assert.AreEqual(a, b);
+        }
+    }
+}
